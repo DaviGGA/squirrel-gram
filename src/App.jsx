9bd448231c7fa203184e3c73639a05e32bf34002
Davi <davigregorio.a@hotmail.com>
2023-07-10 11:14:01 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Login from './pages/Login'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Login></Login>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+function Login() {
+    return (
+       <div className='container'>
+            <div className='white-card'>
+                <h1 className='title'>Bem-vindo de Volta!</h1>
+                <p className='sub-title'>
+                    Reconecte-se com sua comunidade online e compartilhe 
+                    momentos especiais com amigos e familiares
+                </p>
+            </div>
+       </div>
+    )
+}
+
+export default Login;
